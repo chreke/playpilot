7544c10c1d4bd08b60ccfd06216117674847a645
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2018-03-07 16:48:25 +0100

    Add Media model

--- pp/models.py
+++ pp/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Media(models.Model):
+    title = models.CharField(max_length=256)
+    imdb_url = models.URLField(max_length=128)
+    year = models.PositiveIntegerField()
+    description = models.TextField()
+    season = models.IntegerField(blank=True, null=True)
+    added_by = settings.AUTH_USER_MODEL
